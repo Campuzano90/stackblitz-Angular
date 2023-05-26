e8da741ab2e82373affe77e21b7fcc7c70096b84
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-26 17:25:02 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'bases';
+  public title: string = 'Mi primera app en Angular';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
