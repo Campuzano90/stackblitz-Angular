4fde99140a4794b75908712595115f60066081d7
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-26 17:38:22 -0600

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CounterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
